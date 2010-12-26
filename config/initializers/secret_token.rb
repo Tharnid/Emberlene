04778adddcab36062ab3f90ef77d1ce7d916b796
Tharnid <jeffmsmithdev@gmail.com>
2010-12-26 12:22:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emberlene::Application.config.secret_token = '9de9eb39d00392506e6e099ad0150abc440ac31bf4d8ca6676dece2e7a8eb0a8e5d8fc26df0d8ea2a51585e033b8b18a74ddff680b57be64b7803ea9891ee831'
