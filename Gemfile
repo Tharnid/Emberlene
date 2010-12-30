38e239c850dd91a96d542a6030b28e03a57b1e59
Tharnid <jeffmsmithdev@gmail.com>
2010-12-29 20:00:40 -0500

    Done with user edit/update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.3'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'annotate-models', '1.0.4'
 gem 'gravatar_image_tag', '0.1.0'
+gem 'will_paginate', '3.0.pre'
 
 group :test do
 	gem 'factory_girl_rails', '1.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,63 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-    @title = "Sign up"
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
-
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def new
+    @user  = User.new
+    @title = "Sign up"
   end
   
   def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Emberlene!!!"
-      redirect_to @user
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
     else
       @title = "Sign up"
       render 'new'
     end
   end
+  
+  def edit
+    @user  = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user  = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => { :success => "Profile updated." }
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id])
+    flash[:success] = "User terminated!!!"
+    redirect_to users_path
+  end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
